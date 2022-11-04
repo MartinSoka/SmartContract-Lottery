f4b173d85e0bdc40d61191991767546bb819f0cb
Martin Soka <m.soka@tricentis.com>
2022-11-04 15:36:14 +0100

    implement chainlink VRF

--- contracts/Loterry.sol
+++ contracts/Loterry.sol
@@ -4,18 +4,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-error Lottery__InsufficientFunds();
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
-contract Lottery {
+error Lottery__InsufficientFunds();
+error Lottery__TransactionFailed();
+
+contract Lottery is VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface private immutable i_coordinator;
     uint256 private immutable i_entranceFee;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
 
     //if one of them wins we need to pay them that's why its payable
     address payable[] private players;
 
-    event LotteryEnter(address indexed player);
+    /* Lottery fields */
+    address s_recentWinner;
 
-    constructor(uint256 entranceFee) {
+    event LotteryEnter(address indexed player);
+    event RequestedLotteryWinner(uint256 requestId);
+    event WinnerPicked(address indexed winner);
+
+    constructor(
+        address vrfCoordinator,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
+        i_coordinator = VRFCoordinatorV2Interface(vrfCoordinator);
         i_entranceFee = entranceFee;
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterLottery() public payable {
@@ -25,11 +50,44 @@ contract Lottery {
         emit LotteryEnter(msg.sender);
     }
 
+    function requestRandomWinnner() external {
+        uint256 requestId = i_coordinator.requestRandomWords(
+            i_gasLane, //gasLane _ max  gas price you are willing to pay for a reques
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+
+        emit RequestedLotteryWinner(requestId);
+    }
+
+    function fulfillRandomWords(uint256, uint256[] memory randomWords)
+        internal
+        override
+    {
+        uint256 lotteryWinnerIndex = randomWords[0] % players.length;
+        address payable recentWinner = players[lotteryWinnerIndex];
+        s_recentWinner = recentWinner;
+
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+
+        if (!success) {
+            revert Lottery__TransactionFailed();
+        }
+
+        emit WinnerPicked(recentWinner);
+    }
+
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
     function getPlayer(uint256 index) public view returns (address) {
         return players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
